c036d7a355f04d3654f7c378a04b41ef7edb8646
henrique <henriquebobinas01@gmail.com>
2026-04-07 19:43:15 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿string fraseDigitada, fraseMemeToNervoso;
+Console.Write("Usuário, o que você pensa sobre mim? ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseMemeToNervoso =
+    $"{fraseDigitada.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(fraseMemeToNervoso);
